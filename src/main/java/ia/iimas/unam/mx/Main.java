856939649259b79b02e9c7fb47b31c13b25c9bae
djanr2 <djanr2@gmail.com>
2024-08-27 19:12:52 -0600

    20240827 add menu Sccaner class

--- src/main/java/ia/iimas/unam/mx/Main.java
+++ src/main/java/ia/iimas/unam/mx/Main.java
@@ -4,6 +4,7 @@ import ia.iimas.unam.mx.model.Item;
 
 import java.io.*;
 import java.util.PriorityQueue;
+import java.util.Scanner;
 
 public class Main {
 
@@ -15,13 +16,30 @@ public class Main {
     private static String pack_19_file_path = "src/main/resources/ks_19_0";
     private static String pack_10000_file_path = "src/main/resources/ks_10000_0";
 
+    Scanner myObj;
+
 
     public static void main(String[] args) {
         items = new PriorityQueue<>();
         ks = new PriorityQueue<>();
         Item i=null;
         //ordered by heuristic
-        getArrayFromFile(pack_19_file_path, items);
+
+        Scanner scanner = new Scanner(System.in);
+        System.out.println("Introduce el numero del archivo a analizar:");
+        System.out.println("1. ks_19_0");
+        System.out.println("2. ks_10000_0");
+
+        String option = scanner.nextLine();
+
+        if(option.equals("1")){
+            getArrayFromFile(pack_19_file_path, items);
+        }else if (option.equals("2")){
+            getArrayFromFile(pack_10000_file_path, items);
+        }else{
+            System.out.println("Opcion no valida");
+            System.exit(0);
+        }
 
         while (!items.isEmpty()) {
             i = items.poll();
